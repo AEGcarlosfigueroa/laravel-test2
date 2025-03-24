b0d554da5f6a1f133a259e2d871fe57387957f57
AEGcarlosfigueroa <carlos.figueroa@ikasle.aeg.eus>
2025-03-24 09:59:51 +0100

    [ADD] correct routes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::apiResource('potion', PotionController::class);
-Route::apiResource('ingredients', IngredientController::class);
-Route::apiResource('wizards', WizardController::class);
+Route::apiResource('api/potions', PotionController::class);
+Route::apiResource('api/ingredients', IngredientController::class);
+Route::apiResource('api/wizards', WizardController::class);
 
 
 
